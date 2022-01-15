c14fe0dfde8a13c51f839d4676f5eaff5251deb9
Lulu\Rara <shalankocho@gmail.com>
2022-01-16 01:02:08 +0900

    [3の倍数]を学習しました。HTMLに付け忘れ注意

--- 01_テキスト/01_JavaScriptの関数/main.js
+++ 01_テキスト/01_JavaScriptの関数/main.js
@@ -15,5 +15,17 @@ const plus1 = function (x) {
 
 // 練習問題３
 // ① ２つの引数を受け取り、それらをかけ合わせた値を返す関数 multiply
-
+const multiply = function (a, b) {
+  return a * b
+}
+multiply(6, 7)
 // ② ２つの引数を受け取り、それらを足し合わせた結果を四捨五入した値を返す関数 plusRound
+const plusRound = function (x, y) {
+  return Math.round(x + y)
+}
+
+const alertTime = function () {
+  alert(new Date().toLocaleString())
+}
+
+alertTime()
