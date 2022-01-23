9c0e8c2568ea13c55d7b52398952a078ffaf7c51
Lulu\Rara <shalankocho@gmail.com>
2022-01-23 17:17:58 +0900

    [Trello (保存機能なし)]。問題解決しました。

--- 02_レシピ/02_Trello（保存機能なし）/main.js
+++ 02_レシピ/02_Trello（保存機能なし）/main.js
@@ -2,15 +2,18 @@ const inputElement = document.getElementById("input-todo")
 const container = document.getElementById("cards-container")
 const addButton = document.getElementById("add-button")
 
+// 追加ボタンを押したとき カードの作成→入力を空にして次の入力ができるように。
 addButton.onclick = function () {
   const card = createCard(inputElement.value)
   container.append(card)
   inputElement.value = ""
+}
 
-  const createCard = function (text) {
-    const card = document.createElement("div")
-    card.className = "card"
-  }
+// カードを入れるhtmlの場所→テキスト表示場所→削除ボタン の作成
+const createCard = function (text) {
+  const card = document.createElement("div")
+  card.className = "card"
+
   const todo = document.createElement("div")
   todo.className = "todo"
   todo.textContent = text
