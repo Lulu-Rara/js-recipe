5831d7e01f111ffa347339d67cc71044aa7b91d6
Lulu\Rara <shalankocho@gmail.com>
2022-01-29 16:57:46 +0900

    ■と〇の発展を学習しました。

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,5 +1,16 @@
 const figure = document.getElementById("figure")
 
-figure.onclick = function () {
-  figure.classList.toggle("rounded")
+figure.onmouseover = function () {
+  if (figure.classList.contains("square")) {
+    figure.classList.replace("square", "rounded")
+  } else if (figure.classList.contains("rounded")) {
+    figure.classList.replace("rounded", "triangle")
+  } else {
+    figure.classList.replace("triangle", "square")
+  }
 }
+
+//onouseOver	マウスが乗った時に発生
+//onMouseOut	マウスが離れた時に発生
+//onMouseUp	マウスで押していたのを離した時に発生
+//onMouseDown	マウスで押した時に発生
