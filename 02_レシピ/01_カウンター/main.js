4c2fb7a660473db16c05718b81042f1cd1d22510
Lulu\Rara <shalankocho@gmail.com>
2022-01-29 16:11:19 +0900

    [カウンター]発展1のみやりました

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,8 +1,14 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const doubleButton = document.getElementById("double-button")
 
 let count = 0
 plusButton.onclick = function () {
   count += 1
+  display.textContent = count
+}
+
+doubleButton.onclick = function () {
+  count *= 2
   display.textContent = count
 }
